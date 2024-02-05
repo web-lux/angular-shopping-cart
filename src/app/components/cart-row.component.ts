c125335ccbc60f1b26b0fde71b153f243e3133d1
web-lux <web-lux@outlook.fr>
2024-02-05 01:09:38 +0100

    can modify quantity of items from cart page

--- src/app/components/cart-row.component.ts
+++ src/app/components/cart-row.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import CartItem from '../interfaces/cartInterface';
+import { CartService } from '../services/cartService'; 
 
 @Component({
   selector: '[app-cart-row]',
@@ -10,5 +11,18 @@ import CartItem from '../interfaces/cartInterface';
   styleUrl: './cart-row.component.scss'
 })
 export class CartRowComponent {
+  cartService: CartService = inject(CartService);
   @Input() cartItem!: CartItem;
-}
+
+  increaseQuantity() {
+    this.cartService.modifyItemQuantity(this.cartItem.id, this.cartItem.quantity + 1)
+  };
+
+  decreaseQuantity() {
+    if (this.cartItem.quantity === 1) {
+      this.cartService.deleteItemInCart(this.cartItem.id);
+      return
+    }
+    this.cartService.modifyItemQuantity(this.cartItem.id, this.cartItem.quantity - 1)
+  };
+};
--- src/app/services/cartService.ts
+++ src/app/services/cartService.ts
@@ -15,5 +15,23 @@ export class CartService {
 
     addToCart(cartItem :CartItem):void {
         this.cartSubject.next([...this.cartSubject.getValue(), cartItem]);
-;    };
+    };
+
+    deleteItemInCart(id: number): void {
+        const prev = this.cartSubject.getValue();
+        this.cartSubject.next(prev.filter((item) => item.id !== id));
+    };
+
+    modifyItemQuantity(id: number, quantity: number): void {
+        const prevCart = this.cartSubject.getValue();
+        const prevItem: CartItem | undefined = prevCart.find(item => item.id === id);
+
+        if (prevItem) {
+            const filteredCart = prevCart.filter((item) => item.id !== id);
+            const newItem = { ...prevItem, quantity: quantity };
+            this.cartSubject.next([...filteredCart, newItem]);
+        } else {
+            console.error("Cannot modify the quantity of an item not in cart!")
+        }
+    }
 };
